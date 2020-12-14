b61259ee6a5dff7505814168d0b3052e4b31e6ba
Imfdj <907765782@qq.com>
2020-12-14 18:12:47 +0800

    feat: 添加sentry线上监控；
    添加sentry线上监控；

--- src/main.js
+++ src/main.js
@@ -5,9 +5,18 @@ import App from './App';
 import store from './store';
 import router from './router';
 import './plugins';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
 if (process.env.NODE_ENV === 'preview') {
   const { mockXHR } = require('../mock/static');
   mockXHR();
+}
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'http://c13799e6f0cd4fb8bf999763badf8bc8@sentry.imfdj.top/3',
+    integrations: [new Integrations.Vue({Vue, attachProps: true})],
+  });
 }
 
 Vue.config.productionTip = false;
