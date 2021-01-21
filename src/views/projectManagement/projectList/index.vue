68f978df6ecd5b2511ba40cc2556a965f87b84bb
Imfdj <907765782@qq.com>
2021-01-21 20:58:40 +0800

    feat: 项目管理框架搭建完成；
    项目管理框架搭建完成；

---
+++ src/views/projectManagement/projectList/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="project-lit">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane v-for="(item, index) in titles" :key="index" :label="item" :name="(index + 1).toString()">{{
+        item
+      }}</el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+  import store from '@/store';
+
+  export default {
+    name: 'ProjectList',
+    data() {
+      return {
+        activeName: '1',
+        titles: [],
+      };
+    },
+    created() {
+      const path = this.$route.path;
+      this.activeName = path.substring(path.lastIndexOf('/') + 1);
+    },
+    mounted() {
+      const routes = store.getters['routes/routes'];
+      const projectRouter = routes && routes.find(item => item.id === 27);
+      this.titles = projectRouter && projectRouter.children.map(item => item.title);
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .project-lit {
+    padding: 20px;
+  }
+</style>
