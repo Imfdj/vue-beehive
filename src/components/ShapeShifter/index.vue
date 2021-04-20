54e435a7d97a4ab1bde3f8a60d77e701b366b1bb
Imfdj <907765782@qq.com>
2021-04-20 16:11:09 +0800

    feat: shape-shifter图片前缀；
    shape-shifter图片前缀；

--- src/components/ShapeShifter/index.vue
+++ src/components/ShapeShifter/index.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <canvas ref="canvas" class="canvas"></canvas>
-    <img v-show="false" ref="logo" src="@/assets/logo.png" alt="" />
-  </div>
+  <canvas ref="canvas" class="canvas"></canvas>
 </template>
 
 <script>
@@ -20,17 +17,6 @@
       },
     },
     mounted() {
-      // 设置icon图片前缀路径
-      console.log(33);
-      console.log(33);
-      console.log(33);
-      console.log(this.$refs.logo);
-      console.log(this.$refs.logo.src);
-      console.log(this.$refs.logo.target?.src);
-      const src = this.$refs.logo.src || this.$refs.logo.target?.src || '';
-      const publicPath = src.substring(0, src.indexOf('static/img'));
-      console.log(publicPath);
-      S.ShapeBuilder.setPublicPath(publicPath);
       S.init(this.$refs.canvas);
 
       // S.UI.simulate('Shape|Shifter|Type|to start|#icon thumbs-up|#countdown 3||');
